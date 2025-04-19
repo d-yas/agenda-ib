c7a0e4e7f7e3e14ab50fdbf66ec110537ca636f4
d <denizcanyas@gmail.com>
2025-04-19 12:10:52 +0300

    vercel fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FineDiningAgenda from "./components/FineDiningAgenda";
 import "./index.css";
-import BlankView from './components/BlankView';
+
 
 
 function App() {
   return (
       <Routes>
         <Route path="/:guestId?" element={<FineDiningAgenda />} />
-        <Route path="/" element={<BlankView />} />
       </Routes>
   );
 }
